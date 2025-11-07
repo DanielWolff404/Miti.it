23c3958da92f43e3135038deaaa8f35ecfdf8600
Daniel Wolff Miti <daniel.wolff.dk@gmail.com>
2025-11-07 20:10:05 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navabr from './components/Navabr'
+
+const App = () => {
+  return (
+  <manin >
+   <Navabr />
+  </manin>
+  )
+}
+
+export default App
---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+const navlinks = [
+  { label: "store", href: "#" },
+  { label: "Mac", href: "#" },
+  { label: "iPhone", href: "#" },
+  { label: "Watch", href: "#" },
+  { label: "Vision", href: "#" },
+  { label: "AirPods", href: "#" },
+];
+export { navlinks };
